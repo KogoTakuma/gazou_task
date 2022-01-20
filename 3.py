d75cbe948b31739d138118f9235303881b1aeeda
KogoTakuma <1358takuma@gmail.com>
2022-01-21 08:29:56 +0900

    1/21

--- 3.py
+++ 3.py
@@ -20,16 +20,10 @@ global outputE
 global X
 global Y
 global outputy
-global minibatch_input_x,sigmoid_input_x,middle_input
-global w1,w2,b1,b2
-global result12
 
 def sigmoid(a):
-    a = a
     s = 1 / (1 + np.exp(-a))
     return s
-def numpy_frompyfunc(l, f):
-    return np.frompyfunc(f, 1, 1)(l)
 
 def softmax(input_a):
   max_a = np.max(input_a)
@@ -42,7 +36,6 @@ def softmax(input_a):
   return a
 
 def randommaker(seed):
-    global b1,b2,w1,w2
     np.random.seed(seed)
     b1 = np.random.normal(0, 1/CELL_OF_AROW, Node_1)
     b1 = np.reshape(b1,[Node_1,1])
@@ -50,18 +43,14 @@ def randommaker(seed):
     b2 = np.reshape(b2,[Node_2,1])
     w1 = np.reshape((np.random.normal(0, 1/CELL_OF_AROW, Node_1*CELL_OF_AROW)),([Node_1,CELL_OF_AROW]))
     w2 = np.reshape((np.random.normal(0, 1/Node_1, Node_1*Node_2)),([Node_2,Node_1]))
-   
+    return w1,b1,w2,b2
 
-def input_layer(x_1):
-  global b1,w1,sigmoid_input_x
+def input_layer(x_1,w1,b1):
   y1 = np.empty(0)
-  result = np.empty(0)
-  sigmoid_input_x = np.empty(0)  
-  y1 = np.dot(w1,np.transpose(x_1)) + b1
+  y1 = np.dot(w1,x_1) + b1
   return y1
 
-def middle_layer(y_1):
-  global b2,w2
+def middle_layer(y_1,w2,b2):
   a_k = np.empty(0)
   a_k = np.dot(w2,y_1) + b2
   return a_k 
@@ -69,37 +58,38 @@ def middle_layer(y_1):
 def output_layer(a_k): 
   return softmax(a_k)
 
-def get_minibatch(num):
-  global X, Y, outputy,result12,minibatch_input_x,middle_input,sigmoid_input
-  x = np.random.choice(NUMBER_OF_PICTURES, BATCH_SIZE)#0~60000の中から100個の数字をランダムに選ぶ  
+def get_minibatch(X,Y,num,w1,b1,w2,b2):
+  x = np.random.choice(NUMBER_OF_PICTURES, BATCH_SIZE)#0~60000の中から200個の数字をランダムに選ぶ  
   result = 0
   sigmoid_input = np.empty(0)
   minibatch_input_x = np.empty(0)
   middle_input = np.empty(0)
   result12 = np.empty(0)
+  row12 = np.empty(0)
   for num in range(BATCH_SIZE):  
-    x1 = np.reshape(X[x[num]], (1,CELL_OF_AROW )) #画像を取り出し786次元ベクトルにする。
+    x1 = np.reshape(X[x[num]], (CELL_OF_AROW,1)) #画像を取り出し786次元ベクトルにする。
     minibatch_input_x = np.append(minibatch_input_x,x1)
     y1 = Y[x[num]]#答えの数
-    b=input_layer(x1)#入力層
+    b=input_layer(x1,w1,b1)#入力層
     sigmoid_input = np.append(sigmoid_input,b)
-    sigmoided_output = numpy_frompyfunc(b,sigmoid)
+    sigmoided_output = sigmoid(b)
     result_sigmoided = sigmoided_output
     middle_input = np.append(middle_input,result_sigmoided)
-    c=middle_layer(result_sigmoided)#中間層
+    c=middle_layer(result_sigmoided,w2,b2)#中間層
     d=output_layer(c)#出力層
     e=-math.log(d[y1])#クロスエントロピー誤差を求める
-    row12 = d - np.eye(10)[y1]
+    row12 = np.reshape((d - np.eye(10)[y1]),[1,Node_2])
     result += e
     result12 = np.append(result12,row12)
   result = result/BATCH_SIZE 
   outputy = result 
-  result12 = np.transpose(np.reshape(result12,(BATCH_SIZE,Node_2)))/BATCH_SIZE 
+  result12 = np.transpose(np.reshape(result12,[BATCH_SIZE,Node_2]))/BATCH_SIZE 
   minibatch_input_x = np.transpose(np.reshape(minibatch_input_x,[BATCH_SIZE,CELL_OF_AROW]))
-  return result
+  middle_input = np.transpose(np.reshape(middle_input,[BATCH_SIZE,Node_1]))
+  return result,result12,minibatch_input_x,middle_input
 
 def caluculate_16(w,ey):
-  return np.transpose(np.dot(np.transpose(w),ey))
+  return np.dot(np.transpose(w),ey)
 
 def caluculate_17(ey,x):
   return np.dot(ey,np.transpose(x))
@@ -117,32 +107,32 @@ def caluculate_20(y,ey):
     onelineey = np.reshape(ey,[Node_1*BATCH_SIZE,1])
     ones =np.reshape((np.ones(Node_1*BATCH_SIZE)),[Node_1*BATCH_SIZE,1])
     result = [x * y for (x, y) in zip((ones - oneliney), [x * y for (x, y) in zip(onelineey, oneliney)])]
-    return np.transpose(np.reshape(result,[BATCH_SIZE,Node_1]))
-def renew_parameter2(EnW2,Enb2):
-  global w1,w2,b1,b2
-  w2 = w2 - LR*EnW2 #22
-  b2 = b2 - np.reshape(LR*Enb2,[Node_2,1]) #24
-def renew_parameter1(EnW1,Enb1,):
-  global w1,w2,b1,b2
-  w1 = w1 - LR*EnW1 #21
-  b1 = b1 - np.reshape(LR*Enb1,[Node_1,1]) #23
+    return np.reshape(result,[Node_1,BATCH_SIZE])
+def renew_parameter2(EnW2,Enb2,w2,b2):
+  renewed_w2 = w2 - LR*EnW2 #22
+  renewed_b2 = b2 - np.reshape(LR*Enb2,[Node_2,1]) #24
+  return renewed_w2,renewed_b2
+def renew_parameter1(EnW1,Enb1,w1,b1):
+  renewed_w1 = w1 - LR*EnW1 #21
+  renewed_b1 = b1 - np.reshape(LR*Enb1,[Node_1,1]) #23
+  return renewed_w1, renewed_b1
 
-
-randommaker(1)#初期の重みを乱数で生成
+w1_new,b1_new,w2_new,b2_new  = randommaker(1)#初期の重みを乱数で生成
 X = mnist.download_and_parse_mnist_file("train-images-idx3-ubyte.gz")
 Y = mnist.download_and_parse_mnist_file("train-labels-idx1-ubyte.gz")
 for i in range(EPOCH):
-    for num in range(int(NUMBER_OF_PICTURES/BATCH_SIZE)):  
-        result = get_minibatch(num)
-        a=caluculate_16(w2,result12)
-        b=caluculate_17(result12,np.reshape(middle_input,[Node_1,BATCH_SIZE]))
-        c=caluculate_18(result12)
-        result16=caluculate_16(w2,result12)
-        renew_parameter2(b,c)
-        result20 = caluculate_20(middle_input,result16)
-        e=caluculate_17(result20,minibatch_input_x)
-        f=caluculate_18(result20)
-        renew_parameter1(e,f)
-    
-        print(i,"エポック クロスエントロピー誤差 "+str(result))    
+    for num in range(int(NUMBER_OF_PICTURES/BATCH_SIZE)):    
+        w1_old,b1_old,w2_old,b2_old = w1_new,b1_new,w2_new,b2_new 
+        result_cross_entropy_loss,result12,minibatch_input_array,middle_input_array = get_minibatch(X,Y,num,w1_old,b1_old,w2_old,b2_old)
+        result16_2=caluculate_16(w2_old,result12)
+        result17_2=caluculate_17(result12,middle_input_array)
+        result18_2=caluculate_18(result12)
+        w2_new,b2_new = renew_parameter2(result17_2,result18_2,w2_old,b2_old)
+        result20 = caluculate_20(middle_input_array,result16_2)
+        result17_1=caluculate_17(result20,minibatch_input_array)
+        result18_1=caluculate_18(result20)
+        w1_new,b1_new = renew_parameter1(result17_1,result18_1,w1_old,b1_old)
+        print(i,"エポック クロスエントロピー誤差 "+str(result_cross_entropy_loss),num)    
 print("end")    
+np.savez('/Users/t_kogo/Desktop/gazou_task/learningdata.npz',w1=w1_old,b1=b1_old,w2=w2_old,b2=b2_old)
+print("saved")
